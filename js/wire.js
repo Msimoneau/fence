25b5d6bf9b1c900091597bfc18f96181839f2b60
Martin simoneau <martin1.simoneau@gmail.com>
2016-08-14 11:29:46 -0400

    Refactoring

--- js/wire.js
+++ js/wire.js
@@ -1,6 +1,5 @@
 jQuery(document).ready(function() {
    var $zoom;
-   $("[name='toggle-wireframe']").bootstrapSwitch();
    $('input[name="toggle-wireframe"]').on('switchChange.bootstrapSwitch', function(event, state) {
      if (state){
         $zoom = $('.zoom').magnify();
@@ -11,5 +10,6 @@ jQuery(document).ready(function() {
 });
 
 jQuery( window ).load(function() {
+   $("[name='toggle-wireframe']").bootstrapSwitch();
    $('.control').show(600);
 });
